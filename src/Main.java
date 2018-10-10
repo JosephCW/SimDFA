d56a2b7b85b6fe7f5f2d6d0929ffd58341ed8e00
Joseph Watts <josephcw@live.com>
2018-10-10 11:31:50 -0500

    Initial completed version

--- src/Main.java
+++ src/Main.java
@@ -14,13 +14,13 @@ public class Main {
 		int numberOfStates = keyboardInput.nextInt();
 		System.out.println();
 		System.out.println("Enter all symbols of your alphabet delineated by a period. (.)");
-		String userAlphabet = keyboardInput.nextLine();
+		String userAlphabet = keyboardInput.next();
 		
 		if (!userAlphabet.contains(".")) {
 			System.out.println("You must have more than one symbol in your alphabet!");
 			System.exit(0);
 		}
-		String[] userAlphabetArr = userAlphabet.split(".");
+		String[] userAlphabetArr = userAlphabet.split("\\.");
 		
 		// loop through all of the supposed states 
 		// and ask where they go using each character of the alphabet. 
@@ -29,8 +29,7 @@ public class Main {
 			State tmpState = new State(i, false);
 			states.add(tmpState);
 		}
-		
-		
+
 		System.out.println("Please enter the resulting state number following the listed transition for each state.");
 		for (State state : states) {
 			// for each state
@@ -41,56 +40,53 @@ public class Main {
 				System.out.print("\tq" + state.getStateId() + ", " + userAlphabetArr[i] + ": ");
 				int stateNumber = keyboardInput.nextInt();
 				state.addTransition(userAlphabetArr[i], states.get(stateNumber));
+				System.out.println();
 			}
 		}
 		
+		System.out.println("Please enter the final states seperated by a period. (.)");
+		System.out.println("Ex. 0.2.5");
+		keyboardInput.nextLine();
 		
+		String finalStates = keyboardInput.nextLine();
 		
+		if(!finalStates.contains(".")) {
+			
+			System.out.println(finalStates);
+			
+			states.get(Integer.valueOf(finalStates)).setAsFinalState(true);
+		} else {
+			String[] finalStatesArr = finalStates.split("\\.");
+			for (int i = 0; i < finalStatesArr.length; i++) {
+				states.get(Integer.valueOf(finalStatesArr[i])).setAsFinalState(true);
+			}
+		}
 		
+		System.out.println("Please enter the string to test against");
+		String testString = keyboardInput.nextLine();
 		
+		System.out.println("Test String: " + testString);
 		keyboardInput.close();
 		
-		
-		int numberStates = 4;
-		Set<String> alphabet = new HashSet<String>();
-		alphabet.add("a");
-		alphabet.add("b");
-		
-		
-		for (int i = 0; i < numberStates; i++) {
-			State tmpState = new State(i, false);
-			states.add(tmpState);			
-		}
-		states.get(numberStates - 1).setAsFinalState(true);
-		
-		String stringToTest = "aba";
-		// each state needs a transition.
-		// Q0
-		states.get(0).addTransition("a", states.get(1));
-		states.get(0).addTransition("b", states.get(0));
-		// Q1
-		states.get(1).addTransition("a", states.get(0));
-		states.get(1).addTransition("b", states.get(2));
-		// Q2
-		states.get(2).addTransition("a", states.get(3));
-		states.get(2).addTransition("b", states.get(0));
-		// Q3
-		states.get(3).addTransition("a", states.get(3));
-		states.get(3).addTransition("b", states.get(3));
-		
-		// String has to have aba in it to pass.
-		
+		List<String> statePrintout = new ArrayList<>();
 		State currentState = states.get(0);
-		for (int i = 0; i < stringToTest.length(); i++) {
-			// Startings state q0.
-			String tmpString = stringToTest.substring(i, i+1);
-			System.out.println(i + ": " + tmpString);
-			System.out.println("Current State: q" + currentState.getStateId());
-			currentState = currentState.getNextState(tmpString);
-		}
-		System.out.println("Current State: q" + currentState.getStateId());
-		System.out.println("Is accepted?: " + currentState.isFinalState());
+		int lastState = currentState.getStateId();
+		for (int i = 0; i < testString.length(); i++) {
+	           String tmpString = testString.substring(i, i+1);
+	           currentState = currentState.getNextState(tmpString);
+	           statePrintout.add("q" + lastState + ", " + tmpString + " -> " + "q" + currentState.getStateId());
+	           lastState = currentState.getStateId();
+	    }
 		
+		if (currentState.isFinalState()) {
+			System.out.println("This string is accepted!");
+		} else {
+			System.out.println("This string is not accepted.");
+		}
+		
+		for (String printout : statePrintout) {
+			System.out.println(printout);
+		}
 		
 	}
 }
